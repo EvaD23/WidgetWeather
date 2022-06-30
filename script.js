28a368e83a917ba267a627bc7e8c0de888ab13e6
Евгения Лазарева <evgenialazareva@MacBook-Air-Evgenia.local>
2022-07-01 00:10:20 +0300

    Add button

--- script.js
+++ script.js
@@ -8,17 +8,28 @@ function timeUpdate() {
 
 timeUpdate();
 
+function weatherUpdate() {
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=59.9386&lon=30.3141&appid=b88bba09365ef63b8a7e7c563932d180&units=metric')
+        .then(response => response.json())
+        .then(json => {
+            let weather = document.querySelector('.weather');
+            let temp = Math.round(json.main.temp);
+            if (temp > 0) {
+                temp = '+' + temp;
+            }
+            temp += '&deg;C'
+            weather.innerHTML = temp;
+        });
+}
 
-fetch('https://api.openweathermap.org/data/2.5/weather?lat=59.9386&lon=30.3141&appid=b88bba09365ef63b8a7e7c563932d180&units=metric')
-    .then(response => response.json())
-    .then(json => {
-        let weather = document.querySelector('.weather');
-        let temp = Math.round(json.main.temp);
-        if (temp > 0) {
-            temp = '+' + temp;
-        }
-        temp += '&deg;C'
-        weather.innerHTML = temp;
+function weatherRefresh() {
+    weatherUpdate();
+    setTimeout(weatherRefresh, 3600000);
+}
+weatherRefresh();
 
-    });
+const btn = document.querySelector('button');
+btn.addEventListener('click', weatherUpdate);
+
+
 
